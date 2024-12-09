229390d1d080fd0b679aa212766add9babe1f1ec
KrishnanProvility <krishnan@provility.com>
2024-12-09 11:15:43 +0530

    Complete Day1

---
+++ Day1/exercise.js
@@ -0,0 +1,58 @@
+//exercise 1:
+
+    //this  is singleline command.
+
+//exercise 2:
+    
+    //Welcome to 30Days Of javaScript.
+
+//exercise 3:
+
+     /* comments can make code readable ,
+     easy to reuse and informations*/
+
+//exercise 4:
+
+var Name ="krishnan" ;//string.
+var boolean = true ;  //boolean.
+var familyName;
+var age =null;
+
+//exercise 5:
+console.log(typeof(Name));
+console.log(typeof(boolean));
+console.log(typeof(familyName));
+console.log(typeof(age));
+
+//exercise 6:
+
+var firstName;
+var lastName;
+var group;
+var comapanyName;
+
+//exercise 7:
+
+var firstName ="krishnan";
+var lastName ="elumalai";
+var group = "computer Science";
+var comapanyName ="Provility";
+
+//exercise 8:
+
+var firstName ="krishnan";
+var lastName ="elumalai";
+var maritalStatus =false;
+var country ="india";
+var age =25;
+
+//exercise 9:
+
+var firstName ="krishnan",lastName ="elumalai",maritalStatus =false,country ="india", age =25;
+
+//exercise 10:
+let myAge =25;
+let yourAge =30;
+
+console.log("i am "+myAge+"years old");
+console.log("you are"+yourAge+"years old");
