75bfedb7589e0bfd444abb9e84452060e78c46ed
KrishnanProvility <krishnan@provility.com>
2024-12-19 18:28:41 +0530

    COmpleted Day-9 Level-1.

--- __debug__/main.js
+++ __debug__/main.js
@@ -1,7 +1,27 @@
-const callback = (n) => {
-    return n ** 2
+const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const products = [
+  { product: 'banana', price: 3 },
+  { product: 'mango', price: 6 },
+  { product: 'potato', price: ' ' },
+  { product: 'avocado', price: 8 },
+  { product: 'coffee', price: 10 },
+  { product: 'tea', price: '' }
+]
+
+
+const findcountry =countries.findIndex((country)=>{
+
+  if( country == "Russia"){
+    return country;
   }
-  function cube(callback, n) {
-    return callback(n) * n
-  }
-  console.log(cube(callback, 4))    
+})
+if(findcountry != -1)
+{
+ console.log(findcountry);
+}
+else{
+  console.log("nothing to print");
+  
+}
